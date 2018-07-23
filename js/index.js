61deb47e7f404e278ffd9fc16b273b2e15f881e7
Гольцев Николай <goltsev.nikolay@triumph.msk.ru>
2018-07-23 18:52:40 +0300

    work...

--- js/index.js
+++ js/index.js
@@ -2,12 +2,21 @@ class CustomTable extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			array: ['id', 'firstname', 'lastname', 'email', 'phone']
+			headers: ['id', 'firstName', 'lastName', 'email', 'phone'],
+			data: []
 		};
+
+		fetch('http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}').then(response => {
+			return response.json();
+		}).then(data => {
+			this.setState({
+				data: data
+			});
+			console.log(data);
+		});
 	}
 
 	render() {
-		console.log(this);
 		return React.createElement(
 			'table',
 			null,
@@ -17,17 +26,37 @@ class CustomTable extends React.Component {
 				React.createElement(
 					'tr',
 					null,
-					this.state.array.map(record => {
+					this.state.headers.map((record, index) => {
 						return React.createElement(
 							'th',
-							null,
+							{ key: index },
 							record
 						);
 					})
 				)
 			),
-			React.createElement('tbody', null)
+			React.createElement(
+				'tbody',
+				null,
+				this.state.data.map(item => {
+					return React.createElement(
+						'tr',
+						null,
+						this.getItem(item)
+					);
+				})
+			)
 		);
+	}
+
+	getItem(item) {
+		return this.state.headers.map((header, index) => {
+			return React.createElement(
+				'td',
+				{ key: index },
+				item[header]
+			);
+		});
 	}
 }
 
--- js/index.jsx.js
+++ js/index.jsx.js
@@ -2,32 +2,51 @@ class CustomTable extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			array: [
+			headers: [
 				'id',
-				'firstname',
-				'lastname',
+				'firstName',
+				'lastName',
 				'email',
 				'phone'
+			],
+			data: [
+
 			]
 		};
+
+		fetch('http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}').then((response) => {
+			return response.json();
+		}).then((data) => {
+			this.setState({
+				data: data
+			});			
+			console.log(data);		
+		});
 	}
 
 	render() {
-		console.log(this);
 		return (
 			<table>
 				<thead>
 					<tr>
-						{this.state.array.map((record) => {
-							return <th>{record}</th>;
+						{this.state.headers.map((record, index) => {
+							return <th key={index}>{record}</th>;
 						})}
 					</tr>
 				</thead>
 				<tbody>
-
+					{this.state.data.map((item) => {
+						return <tr>{this.getItem(item)}</tr>;
+					})}
 				</tbody>
 			</table>
 		);
+	}
+
+	getItem(item) {
+		return this.state.headers.map((header, index) => {
+			return <td key={index}>{item[header]}</td>;
+		});
 	}
 }
 
